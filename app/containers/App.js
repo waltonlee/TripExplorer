6cffb2075f35929b0fd52397d8ba1ae9f8293132
Thad Kerosky <contact@thadk.net>
2016-10-15 04:24:14 -0400

    first pass at integration, svg invisible, force not confirmed

--- app/containers/App.js
+++ app/containers/App.js
@@ -9,7 +9,26 @@ import {
   TextInput,
   Image
 } from "react-native";
+import Svg,{
+    Circle,
+    Ellipse,
+    G,
+    LinearGradient,
+    RadialGradient,
+    Line,
+    Path,
+    Polygon,
+    Polyline,
+    Rect,
+    Symbol,
+    Use,
+    Defs,
+    Stop
+} from 'react-native-svg';
+import Graph from './Graph';
+import {Text as SvgText} from 'react-native-svg';
 import {connect} from 'react-redux';
+import D3 from 'd3';
 const styles = StyleSheet.create({
   app: {
     paddingTop: 40
@@ -17,20 +36,29 @@ const styles = StyleSheet.create({
 });
 
 
+    var width = 960;
+    var height = 500;
 
 /**
- * @desc Your app's content goes here mothafucka
  */
 class App extends Component {
   static propTypes = {};
   static contextTypes = {};
   constructor(props) {
     super(props);
-    this.state = { text: 'Useless Placeholder' };
+    var force = D3.layout.force()
+      .charge(-300)
+      .linkDistance(150)
+      .size([width, height]);
+    this.state = { text: 'Useless Placeholder',
+    nodes: [],
+    links: [],
+    force: force };
 
   }
 
   componentDidMount(){
+    this.firstData();
     var fetchTripAdvisor = function (lat,long,_this) {
       // 42.33141,-71.099396
       console.log(`https://api.tripadvisor.com/api/partner/2.0/map/${lat},${long}?key=89DE2CFC0C1C43978B484B55F9A514EC`)
@@ -72,15 +100,102 @@ class App extends Component {
           <View key={location.location_id} style={{backgroundColor: 'grey'}}>
             <Text style={{color: 'white'}}>{location.name}</Text>
             <Text style={{color: 'white'}}>{location.string}</Text>
-
           </View>
 
         );
       })}
+        <View>
+          <Graph nodes={this.state.nodes} links={this.state.links} force={this.state.force} parentApp={this}/>
+        </View>
       </View>
     );
   }
-}
+  firstData() {
+    // <View className="update" onClick={this.updateData}>update</div>
+    // randomData is loaded in from external file generate_data.js
+    // and returns an object with nodes and links
+    var newState = {nodes: [{
+                x: width / 2,
+                y: height / 2,
+                key: 0,
+                size: 5
+            }, {
+                x: width / 2,
+                y: height / 2,
+                key: 1,
+                size: 6
+            }, {
+                x: width / 2,
+                y: height / 2,
+                key: 2,
+                size: 7
+            }, {
+                x: width / 2,
+                y: height / 2,
+                key: 3,
+                size: 8
+            }
+
+        ], links: [{
+                key: "0,1",
+                size: 3,
+                source: 0,
+                target: 1
+            }, {
+                key: "0,2",
+                size: 2,
+                source: 0,
+                target: 2
+            }, {
+                key: "0,3",
+                size: 1,
+                source: 0,
+                target: 3
+            }]};
+    this.setState(newState);
+  }
+  updateData() {
+    console.log(this.state.nodes);
+    // randomData is loaded in from external file generate_data.js
+    // and returns an object with nodes and links
+    var distances = new Array();
+    var i = 0;
+    var nodes = new Array();
+    var links = new Array();
+
+    var locationsFromApi = this.state.locations && this.state.locations.map(function(location) {
+      distances.push(parseFloat(location.distance, 10))
+      nodes.push({
+          x: width / 2,
+          y: height / 2,
+          key: i,
+          size: 15,
+          name: location.name
+      });
+      if(i > 0) {
+          links.push({
+              key: ("0,"+i),
+              size: Math.pow((13*distances[i]),2),
+              source: 0,
+              target: i
+
+          });
+      };
+      i = i+1;
+    });
+    if(nodes.length > 0 && links.length > 0) {
+        var newState2 = {nodes, links}
+    }
+    if(newState2) {
+        //console.log(newState2);
+        this.setState(newState2);
+
+    }
+  }
+
+};
+
+
 
 export default connect(
   state => {
---
+++ app/containers/Graph.js
@@ -0,0 +1,106 @@
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  Image
+} from "react-native";
+import Svg,{
+    Circle,
+    Ellipse,
+    G,
+    LinearGradient,
+    RadialGradient,
+    Line,
+    Path,
+    Polygon,
+    Polyline,
+    Rect,
+    Symbol,
+    Use,
+    Defs,
+    Stop
+} from 'react-native-svg';
+import {Text as SvgText} from 'react-native-svg';
+import {connect} from 'react-redux';
+const width = 960;
+const height = 500;
+import D3 from 'd3';
+var Graph = React.createClass({
+  componentWillMount() {
+    this.props.force.on('tick', () => {
+      // after force calculation starts, call
+      // forceUpdate on the React component on each tick
+      this.forceUpdate()
+    });
+  },
+
+  componentWillReceiveProps(nextProps) {
+    // we should actually clone the nodes and links
+    // since we're not supposed to directly mutate
+    // props passed in from parent, and d3's force function
+    // mutates the nodes and links array directly
+    // we're bypassing that here for sake of brevity in example
+    this.props.force.nodes(nextProps.nodes).links(nextProps.links);
+
+    this.props.force.start();
+  },
+
+  localUpdateData(node) {
+    this.props.nodes.map(function(n,x) {
+      if (n.key === node.key) {
+        n.selected = true;
+      } else {
+        n.selected = false;
+      }
+    });
+    this.props.parentApp.updateData();
+  },
+
+  render() {
+    // use React to draw all the nodes, d3 calculates the x and y
+    var nodes = this.props.nodes.map((node) => {
+      var transform = 'translate(' + node.x + ',' + node.y + ')';
+      var boundClick = this.localUpdateData.bind(this, node);
+      //transform={transform}
+      return (
+        <View>
+        <G className='node' key={node.key} >
+          <Circle r={node.size} onClick={boundClick} />
+          <SvgText x={node.size + 5} dy='.35em'>{node.name}</SvgText>
+        </G>
+        </View>
+      );
+    });
+    var links = this.props.links.map( (link) => {
+      return (
+        <Line className='link' key={link.key} strokeWidth={link.size}
+          x1={link.source.x} x2={link.target.x} y1={link.source.y} y2={link.target.y} />
+      );
+    });
+
+
+
+    return (
+      <View>
+        <Svg width={width} height={height}>
+          <G>
+            {links}
+            {nodes}
+          </G>
+        </Svg>
+      </View>
+    );
+  }
+});
+
+export default connect(
+  state => {
+    return {
+    }
+  }
+)(Graph);
